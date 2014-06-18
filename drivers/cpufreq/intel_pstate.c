979d6c02088d3136f75bde7d3cf7a534907f8080
Stratos Karafotis <stratosk@semaphore.gr>
2014-06-19 00:01:14 +0300

    cpufreq: intel_pstate: Disable interrupts during MSRs reading
    According to Intel 64 and IA-32 Architectures SDM, Volume 3,
    Chapter 14.2, "Software needs to exercise care to avoid delays
    between the two RDMSRs (for example interrupts)".
    
    So, disable interrupts during reading MSRs IA32_APERF and IA32_MPERF.
    This should increase the accuracy of the calculations.
    
    Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -557,9 +557,12 @@ static inline void intel_pstate_calc_busy(struct cpudata *cpu)
 static inline void intel_pstate_sample(struct cpudata *cpu)
 {
 	u64 aperf, mperf;
+	unsigned long flags;
 
+	local_irq_save(flags);
 	rdmsrl(MSR_IA32_APERF, aperf);
 	rdmsrl(MSR_IA32_MPERF, mperf);
+	local_irq_restore(flags);
 
 	cpu->last_sample_time = cpu->sample.time;
 	cpu->sample.time = ktime_get();
